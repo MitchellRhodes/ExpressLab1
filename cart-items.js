6bf7d1e6ca89cfe475181ca482aa1ea86f0d4892
Mitchell Rhodes <M.Rhodes87@comcast.net>
2021-04-30 21:01:20 -0400

    get cart-items/:id works. need to add 404

--- cart-items.js
+++ cart-items.js
@@ -5,48 +5,45 @@ const cartItems = express.Router();
 cartItems.use(express.json());
 
 
+const items = [
+    {
+        id: 10,
+        product: 'milk',
+        price: 2.99,
+        quantity: 1
+    },
+
+    {
+        id: 1025,
+        product: 'resident evil 8',
+        price: 59.99,
+        quantity: 1
+    },
+
+    {
+        id: 451,
+        product: '4k Tv',
+        price: 299.99,
+        quantity: 1
+    },
+]
+
+
 cartItems.get('/cart-items', (req, res) => {
-    res.status(200).json(
-        [
-            {
-                id: 10,
-                product: 'milk',
-                price: 2.99,
-                quantity: 1
-            },
-
-            {
-                id: 1025,
-                product: 'resident evil 8',
-                price: 59.99,
-                quantity: 1
-            },
-
-            {
-                id: 451,
-                product: '4k Tv',
-                price: 299.99,
-                quantity: 1
-            },
-        ]
-    );
+    res.status(200).json(items);
 });
 
 
 cartItems.get('/cart-items/:id', (req, res) => {
-    res.status(200).json(
-        console.log(res.params.product)
+
+    res.json(
+        items.find(item => item.id === +req.params.id)
     );
     // res.status(404).json(
     //     'ID not found'
     // );
 });
 
-cartItems.get('/cart-items/:id', (req, res) => {
-    res.status(404).json(
-        'ID not found'
-    );
-});
 
 
 
