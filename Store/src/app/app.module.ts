af926264b43a51b4861c41858eef20f685b2f08c
Mitchell Rhodes <M.Rhodes87@comcast.net>
2021-05-23 16:03:38 -0400

    began to add auth0, imported into project and set up domain and clientId

--- Store/src/app/app.module.ts
+++ Store/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
 import { ProductsComponent } from './products/products.component';
 import { AddProductComponent } from './add-product/add-product.component';
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,11 @@ import { AddProductComponent } from './add-product/add-product.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AuthModule.forRoot({
+      domain: 'dev-zrdg9rlj.us.auth0.com',
+      clientId: 'Xl3TqHJgdzY6DqzZ2RCQC4tioDd0PZzH'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
